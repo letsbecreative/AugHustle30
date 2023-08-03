11581b190b5306c866a9277c35da35e48d4338cf
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-08-03 23:31:07 +0530

    Maximum SubArray - LeetCode

---
+++ Day 1/MaximumSubArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int n = arr.length;
+        int maxi = Integer.MIN_VALUE; // maximum sum
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+}
