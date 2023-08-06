ab9a86dd7f44c9d4b8438e0759ba687c2f1bb066
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-08-06 23:33:22 +0530

    Length Of Last Word - GFG

---
+++ Day 4 /LengthOfLastWord.java
@@ -0,0 +1,17 @@
+class Solution 
+{ 
+    int findLength(String s) 
+    { 
+        s = s.trim();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
+
