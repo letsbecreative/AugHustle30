e0a414e2790c3758866d39a3edbeddd585cb7d6a
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-08-09 23:35:41 +0530

    Check if two arrays are equal or not - GFG

---
+++ Day 7/CheckingTwoArrEqualOrNot.java
@@ -0,0 +1,13 @@
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        if(Arrays.equals(A, B))
+            return true;
+        else
+            return false;
+    }
+}
