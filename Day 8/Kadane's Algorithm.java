31170f1f2176b90091fb595b9838f1721befb2d2
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-08-10 23:34:16 +0530

    Create Kadane's Algorithm.java

---
+++ Day 8/Kadane's Algorithm.java
@@ -0,0 +1,19 @@
+class Solution{
+
+    long maxSubarraySum(int arr[], int n){
+        long maxi = Long.MIN_VALUE;
+        long sum =0;
+         for(int i=0; i<n;i++){
+             sum += arr[i];
+             if(sum>maxi){
+                 maxi = sum;
+             }
+    
+             if(sum<0){sum=0;}
+         }
+         
+         return maxi;
+            
+    }
+    
+}
